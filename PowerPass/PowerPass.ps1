cfdce85950f88656f7029f8a84df7468048de27c
chopinrlz <chopinrlz@gmail.com>
2023-10-23 17:09:30 -0400

    Bug in version check

--- PowerPass/PowerPass.ps1
+++ PowerPass/PowerPass.ps1
@@ -2,6 +2,6 @@
 # Copyright 2023 by The Daltas Group LLC.
 # This software is provided AS IS WITHOUT WARRANTEE.
 # You may copy, modify or distribute this software under the terms of the GNU Public License 2.0.
-if( $PSVersion.PSVersion.Major -ne 5 ) {
+if( $PSVersionTable.PSVersion.Major -ne 5 ) {
     Write-Warning "This module can only be used with PowerShell 5.1."
 }
