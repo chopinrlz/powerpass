d8c5c3f4dbcd469562e722e8296770b50ea38693
chopinrlz <chopinrlz@gmail.com>
2024-01-06 14:14:21 -0500

    Parameterized and gated for WinPS

--- util/Dump-CimInfo.ps1
+++ util/Dump-CimInfo.ps1
@@ -1,14 +1,21 @@
 #
 # Common Information Model (CIM) export utility
 #
-# Copyright 2023 by The Daltas Group LLC.
+# Copyright 2023-2024 by The Daltas Group LLC.
 # This software is provided AS IS WITHOUT WARRANTEE.
 # You may copy, modify or distribute this software under the terms of the GNU Public License 2.0.
 #
+param(
+    [string]
+    $Class = "CIM_Processor"
+)
+if( $PSVersionTable.PSVersion.Major -gt 5 ) {
+    throw "This script requires Windows PowerShell 5.1"
+}
 $file = Join-Path -Path $PSScriptRoot -ChildPath "cim.json"
 if( Test-Path $file ) {
-    Remove-Item -Path $file
+    Remove-Item -Path $file -Force
 }
-Get-CimClass | ? CimClassName -eq "CIM_Processor" | % {
+Get-CimClass | ? CimClassName -eq $Class | % {
     Get-CimInstance -ClassName ($_.CimClassName) | ConvertTo-Json -Depth 99 | Out-File "$file" -Append
 }
