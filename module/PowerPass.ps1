3619e89fd93681b4c6ff82009e12c308488b3ce6
chopinrlz <chopinrlz@gmail.com>
2024-01-06 13:51:49 -0500

    Readded PS version check, updated copyright

--- module/PowerPass.ps1
+++ module/PowerPass.ps1
@@ -1,6 +1,8 @@
 # PowerPass startup script
-# Copyright 2023 by The Daltas Group LLC.
+# Copyright 2023-2024 by The Daltas Group LLC.
 # This software is provided AS IS WITHOUT WARRANTEE.
 # You may copy, modify or distribute this software under the terms of the GNU Public License 2.0.
 
-# This startup script is now empty since there are no preconditions to verify
+if( $PSVersionTable.PSVersion.Major -lt 5 ) {
+	Write-Warning "PowerPass requires Windows PowerShell 5.1 or PowerShell 7"
+}
