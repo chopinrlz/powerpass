56b0e674fb2b70e21fe5e1a33bb709934aeb3bc5
chopinrlz <chopinrlz@gmail.com>
2024-03-12 11:36:39 -0400

    Comments and asserts

--- module/Compression.cs
+++ module/Compression.cs
@@ -1,11 +1,25 @@
 using System;
 using System.IO;
 using System.IO.Compression;
-using System.Text;
 
 namespace PowerPass {
+
+	/// <summary>
+	/// Provides a simple API for applying GZip compression to files on disk.
+	/// </summary>
 	public static class Compressor {
+
+		/// <summary>
+		/// Compresses a file on disk using GZip and returns the compressed file as a byte array.
+		/// </summary>
+		/// <param name="file">The full path of the file on disk.</param>
+		/// <returns>The GZip compressed array of bytes for the file.</returns>
 		public static byte[] CompressFromDisk( string file ) {
+			// Asserts
+			if( String.IsNullOrEmpty( file ) ) throw new ArgumentNullException( "file" );
+			if( !File.Exists( file ) ) throw new ArgumentException( "File does not exist" );
+
+			// Load the file and compress it to an in-memory stream
 			using( MemoryStream ms = new MemoryStream() ) {
 				using( FileStream fs = File.OpenRead( file ) ) {
 					using( GZipStream gz = new GZipStream( ms, CompressionMode.Compress ) ) {
@@ -16,7 +30,16 @@ namespace PowerPass {
 			}
 		}
 
+		/// <summary>
+		/// Decompresses a GZip file from a base64 string.
+		/// </summary>
+		/// <param name="text">The base64 text of the compressed file data.</param>
+		/// <returns>The decompressed file bytes.</returns>
 		public static byte[] DecompressFromBase64( string text ) {
+			// Asserts
+			if( String.IsNullOrEmpty( text ) ) throw new ArgumentNullException( "text" );
+
+			// Decompress the data to a byte array
 			using( MemoryStream ms = new MemoryStream() ) {
 				using( MemoryStream cd = new MemoryStream() ) {
 					byte[] data = Convert.FromBase64String( text );
