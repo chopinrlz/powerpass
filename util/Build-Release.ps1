ba87e4514e1531535f4fc656f6615cfa4ca34ba6
chopinrlz <chopinrlz@gmail.com>
2024-04-17 18:47:07 -0400

    Included new code in build utility

--- util/Build-Release.ps1
+++ util/Build-Release.ps1
@@ -54,6 +54,7 @@ $dirPowerPass = Join-Path -Path (Get-Location) -ChildPath "module"
 # Declare each module file for the release and verify presence
 $aesCryptoCs = Join-Path -Path $dirPowerPass -ChildPath "AesCrypto.cs"
 $compressionCs = Join-Path -Path $dirPowerPass -ChildPath "Compression.cs"
+$conversionCs = Join-Path -Path $dirPowerPass -ChildPath "Conversion.cs"
 $extensionsCs = Join-Path -Path $dirPowerPass -ChildPath "Extensions.cs"
 $powerPassCommon = Join-Path -Path $dirPowerPass -ChildPath "PowerPass.Common.ps1"
 $powerPassAesPsd1 = Join-Path -Path $dirPowerPass -ChildPath "PowerPass.Aes.psd1"
@@ -62,7 +63,7 @@ $powerPassDpApiPsd1 = Join-Path -Path $dirPowerPass -ChildPath "PowerPass.DpApi.
 $powerPassDpApiPsm1 = Join-Path -Path $dirPowerPass -ChildPath "PowerPass.DpApi.psm1"
 $powerPassPs1 = Join-Path -Path $dirPowerPass -ChildPath "PowerPass.ps1"
 $statusLoggerCs = Join-Path -Path $dirPowerPass -ChildPath "StatusLogger.cs"
-$powerPassFiles = @($aesCryptoCs,$compressionCs,$extensionsCs,$powerPassAesPsd1,$powerPassAesPsm1,$powerPassDpApiPsd1,$powerPassDpApiPsm1,$powerPassPs1,$statusLoggerCs,$powerPassCommon)
+$powerPassFiles = @($aesCryptoCs,$compressionCs,$extensionsCs,$powerPassAesPsd1,$powerPassAesPsm1,$powerPassDpApiPsd1,$powerPassDpApiPsm1,$powerPassPs1,$statusLoggerCs,$powerPassCommon,$conversionCs)
 foreach( $file in $powerPassFiles ) {
     if( -not (Test-Path $file) ) {
         throw "$file is missing"
