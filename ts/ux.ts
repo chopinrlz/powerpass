f43a40f7f725f632222f449295c07a575a8799f5
chopinrlz <chopinrlz@gmail.com>
2024-06-20 19:32:03 -0400

    Open checks, data binding, typing, comments

--- ts/ux.ts
+++ ts/ux.ts
@@ -1,70 +1,114 @@
 import { PowerPassLocker } from './powerpass';
+import { Secret } from './powerpass';
 
 console.log('debug: loading startup.js');
 
+// Forward declarations for Rivets and jQuery objects
 declare var rivets:any, $:any;
 
-// Load data binding
+// The controller for index.html
 class IndexClass {
     powerpass: PowerPassLocker;
     locker!: PowerPassLocker;
     message: string;
     loaded: boolean;
 
+    /**
+     * Creates the IndexClass instance which implements the front-end for PowerPass
+     */
     constructor() {
-        
         this.powerpass = new PowerPassLocker();
-
         this.message = 'Welcome to the browser edition of PowerPass';
         this.loaded = false;
         rivets.bind($('body'), { data: this });
     }
 
+    /**
+     * Opens your PowerPass Locker from local storage if it is not already open
+     */
     async openLocker() {
-        this.message = 'Fetching your Locker';
-        this.powerpass.init();
-        if( this.powerpass.secrets.length <= 0 ) {
-            this.powerpass.add(this.powerpass.newSecret());
+        if( !(this.loaded) ) {
+            this.message = 'Fetching your Locker';
+            this.powerpass.init();
+            if( this.powerpass.secrets.length <= 0 ) {
+                this.powerpass.add(this.powerpass.newSecret());
+            }
+            this.locker = this.powerpass;
+            this.loaded = true;
+            this.message = 'Ready';
+        } else {
+            this.message = 'Your Locker is already open';
         }
-        this.locker = this.powerpass;
-        this.loaded = true;
-        this.message = 'Ready';
     }
 
+    /**
+     * Closes your PowerPass Locker saving it to local storage if it is currently open
+     */
     async closeLocker() {
-        var locker = this.powerpass.encrypt('testing');
-        localStorage.setItem('powerpass',locker);
-        this.message = 'Welcome to the browser edition of PowerPass';
-        this.loaded = false;
-        (this.locker as any) = undefined;
+        if( this.loaded ) {
+            var locker = this.powerpass.encrypt('testing');
+            localStorage.setItem('powerpass',locker);
+            this.message = 'Welcome to the browser edition of PowerPass';
+            this.loaded = false;
+            (this.locker as any) = undefined;
+        } else {
+            this.message = 'Your Locker is not open';
+        }
     }
 
+    /**
+     * Adds a new Locker secret from the Title, Username, and Password inputs
+     */
     async addSecret() {
         this.message = 'Adding Secret';
         var title = $('#textTitle').val();
         if( title ) {
-            var found = this.locker.secrets.find(s => s.title === title);
-            if( found ) {
-                this.message = 'Secret ' + title + ' already exists'; 
+            if( (typeof this.locker) !== undefined ) {
+                var found = this.locker.secrets.find(s => s.title === title);
+                if( found ) {
+                    this.message = 'Secret ' + title + ' already exists'; 
+                } else {
+                    var secret = this.powerpass.newSecret();
+                    secret.title = title;
+                    var username = $('#textUsername').val();
+                    if( username ) secret.username = username;
+                    var password = $('#textPassword').val();
+                    if( password ) secret.password = password;
+                    this.powerpass.add(secret);
+                    this.locker = this.powerpass;
+                    this.message = 'Locker Secrets: ' + this.powerpass.secrets.length;
+                    this.clearInputs();
+                }
             } else {
-                var secret = this.powerpass.newSecret();
-                secret.title = title;
-                this.powerpass.add(secret);
-                this.locker = this.powerpass;
-                this.message = 'Locker Secrets: ' + this.powerpass.secrets.length;
-                this.clearInputs();
+                this.message = 'Locker is not open';
             }
         } else {
             this.message = 'Secrets must have a unique Title';
         }
     }
-    async revealPw(me:any) {
-        var found = this.locker.secrets.find(s => s.title === me.id);
-        if(found) this.reveal(found);
+
+    /**
+     * Reveals a password on the main page
+     * @param me The clicked Button element with rv-id set to the Title of the secret
+     */
+    async revealPw(me: any) {
+        if( (typeof this.locker) !== undefined ) {
+            var found: Secret | undefined = this.locker.secrets.find(s => s.title === me.id);
+            if(found) this.reveal(found);
+        }
     }
-    reveal(item:any) {
+
+    /**
+     * Inverts the revealed flag on a PowerPass Locker secret
+     * @param item The Secret's password to reveal or hide
+     */
+    reveal(item: Secret) {
         item.revealed = !(item.revealed);
     }
+
+    /**
+     * Erases all the text entered into the Title, Username, and Password boxes
+     */
     clearInputs() {
         $('#textTitle').val('');
         $('#textUsername').val('');
@@ -72,4 +116,5 @@ class IndexClass {
     }
 }
 
+// Create objIndex for the page
 (window as any)["objIndex"] = new IndexClass();
