321d4d1c4eb48bca966497066b92b9bee19511fd
chopinrlz <chopinrlz@gmail.com>
2026-01-27 11:11:27 -0500

    Added verification for import testing

--- test/Test-Import.ps1
+++ test/Test-Import.ps1
@@ -1,4 +1,15 @@
+if( $PSVersionTable.PSVersion.Major -ne 5 ) {
+	throw 'This script requires PowerShell 5.'
+}
 Import-Module PowerPass
+$p = Get-PowerPass
+if( $p ) {
+	if( $p.Implementation -ne 'DPAPI') {
+		throw 'This test module is for the DP API edition of PowerPass.'
+	}
+} else {
+	throw 'PowerPass is not installed. Run Deploy-PowerPass.ps1'
+}
 Clear-PowerPassLocker -Force
 $path = Join-Path -Path $PSScriptRoot -ChildPath "kpdb-import.kdbx"
 $db = Open-PowerPassDatabase -Path $path -MasterPassword (ConvertTo-SecureString "12345" -AsPlainText -Force)
